d3a97d436dd3d2b68a6799c0a186f342980945e8
chengjf <sharpkingdom@gmail.com>
2015-09-17 00:24:34 +0800

    初次提交。

---
+++ src/main/java/com/chengjf/mavendemo/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.chengjf.mavendemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = new PrintWriter(resp.getOutputStream());
+		out.println("<html>");
+		out.println("<head><title>Servlet1</title></head>");
+		out.println("<body>hello world!");
+		out.println("</body></html>");
+		out.close();
+	}
+}
